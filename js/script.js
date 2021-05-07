4f84642d7565acae36e55a798a55b9729ed286e2
edenhailu333@gmail.com <EDENhailu21>
2021-05-07 18:36:44 -0500

    second push

--- js/script.js
+++ js/script.js
@@ -4,53 +4,71 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-/*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
 function showPage(list, page) {
   // create two variables which will represent the index for the first and last student on the page
-   let startIndex = (page * 9) - 9;
-   let endIndex = page * 9;
-   
-  // select the element with a class of `student-list` and assign it to a variable
-  // set the innerHTML property of the variable you just created to an empty string
-  let studList = document.querySelector(".student-list")
- 
-   studList.innerHTML = "";
+  let startIndex = page * 9 - 9;
+  let endIndex = page * 9;
+  let studList = document.querySelector(".student-list");
+
+  // removes previosly displayed students info
+  studList.innerHTML = "";
+
   // loop over the length of the `list` parameter
-   for (let i = 0; i < list.length; i++  ) {
-   if (i>= startIndex && i< endIndex)
-      const studentItem = ` <li class="student-item cf">
+  for (let i = 0; i < list.length; i++) {
+    if (i >= startIndex && i < endIndex) {
+      // create the elements needed to display the student information
+
+      let studentItem = ` <li class="student-item cf">
            <div class="student-details">
-           <img class="avatar" src="${list[i].picture}" alt="Profile Picture">
+           <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
            <h3>${list[i].name.first} ${list[i].name.last}</h3>
-           <span class="email">${list[i]}</span>
+           <span class="email">${list[i].email}</span>
            </div>
            <div class="joined-details">
-         <span class="date">${list[i].registered.date}</span>
+         <span class="date">Joined ${list[i].registered.date}</span>
          </div>
          </li>
-      `
-     };
-  // inside the loop create a conditional to display the proper students
-  // inside the conditional:
-  // create the elements needed to display the student information
-  // insert the above elements
-
-
-
+      `;
+      studList.insertAdjacentHTML("beforeend", studentItem);
+    }
+  }
 }
 
+showPage(data, 1);
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
+   const numOfPages = Math.ceil(list.length / 9);
+   const linkList = document.querySelector(".link-list");
+
+   // removes any previously displayed pagination
+   linkList.innerHTML = "";
+   
+   for (let i = 1; i <= numOfPages; i++) {
+      const button = `<li><button type="button">${i}</button</li>`;
+
+      linkList.insertAdjacentHTML("beforeend", button);
+   }
+
+   document.querySelector("li > button").className = "active";
+
+   linkList.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") {
+         document.querySelector(".active").className = "";
+         e.target.className = "active";
+
+         showPage(list, e.target.textContent);
+      }
+   });
+}
+
+addPagination(data);
 
 // Call functions
